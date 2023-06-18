0ccb0fce1b40a0716146d71a99d312283f89253b
SergiiOrlov <127971805+SergiiOrlov@users.noreply.github.com>
2023-06-18 20:09:13 +0300

    Update tasks-01.js
    solutions of Sergii Orlov

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,9 +1,26 @@
-// https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
-
-function problem(x) {
-  const num = parseFloat(x);
-  if (Number.isNaN(num)) {
-    return "Error";
-  }
-  return num * 50 + 6;
+//Sergii Orlov
+// 1- Messi goals function
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
 }
+// 2- Return Negative
+function makeNegative(num) {
+  return ( Math.sign(num) === 1 ) ? -num : num;
+}
+//3-Grasshopper - Terminal game move function
+function move (position, roll) {
+  return position += roll * 2
+}
+//4-Grasshopper - Personalized Message
+function greet (name, owner) {
+  return (name === owner) ? "Hello boss" : "Hello guest";
+}
+//5- Keep Hydrated!
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+//6 -Opposites Attract
+function lovefunc(flower1, flower2){
+  return (flower1 % 2 != flower2 % 2 ) ? true : false;
+}
+/*---------------------------------------------------------*/
