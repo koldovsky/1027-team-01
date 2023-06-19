1105aac55a493da89bd9722f28e6e7cd6e9f1405
Taras Kravetskyi <ckjym@ukr.net>
2023-06-19 13:21:21 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -112,3 +112,41 @@ function lovefunc(flower1, flower2) {
     return false;
   }
 }
+
+// Taras Kraveyskyi 
+// 1 Messi Goals
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// 2 Make negative
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+ 
+// 3 Game Move 
+function move (position, roll) {
+  return position + roll * 2
+}
+
+// 4 Personalized Message
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
+
+// 5 Keep Hydrated 
+function litres(time) {
+  return ~~(time/2);
+}
+
+// 6 Opposites Attract
+function lovefunc(flower1, flower2) {
+  return (flower1 % 2 === 0) !== (flower2 % 2 === 0);
+}
+
+
+
+
+
+
+
