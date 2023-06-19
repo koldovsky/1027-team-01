cc13b9895299c286eb416577403adc113c43d30b
SergiiOrlov <127971805+SergiiOrlov@users.noreply.github.com>
2023-06-19 17:13:37 +0300

    Update tasks-01.js
    ok

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,27 +1,33 @@
-// 1- Messi goals function
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4 ( Messi goals function )
+
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-// 2 Make negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102 ( Return Negative )
+
 function makeNegative(num) {
   return -Math.abs(num);
 }
 
-//3-Grasshopper - Terminal game move function
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2  ( Terminal game move function )
+
 function move(position, roll) {
   return (position += roll * 2);
 }
-// 4 Personalized Message
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7 (  Personalized Message )
+
 function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
-// 5 Keep Hydrated
+// https://www.codewars.com/kata/582cb0224e56e068d800003c ( Keep Hydrated! )
+
 function litres(time) {
   return ~~(time / 2);
 }
 
-//6 -Opposites Attract
+// https://www.codewars.com/kata/555086d53eac039a2a000083  ( Opposites Attract )
+
 function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
