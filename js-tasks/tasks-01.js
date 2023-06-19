5f7133fcaa54fce2effeb3613ae0956458616b60
Bogdan <bgra4ov@gmail.com>
2023-06-19 10:11:57 +0300

    JS Tasks Done

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -61,3 +61,54 @@ function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
 
+// Grachov Bogdan
+
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4 ( Messi goals function )
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102 ( Return Negative )
+
+function makeNegative(num) {
+  if (num > 0) {
+    return 0 - num;
+  } else {
+    return num;
+  }
+}
+
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2  ( Terminal game move function )
+
+function move(position, roll) {
+  return roll * 2 + position;
+}
+
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7 (  Personalized Message )
+
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
+
+// https://www.codewars.com/kata/582cb0224e56e068d800003c ( Keep Hydrated! )
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083  ( Opposites Attract )
+
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
+  } else if (flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
