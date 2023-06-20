129d618e7a9b8a1e99d184651d56d1c11f6afb7a
GUID <vitalijmatvienko@gmail.com>
2023-06-20 19:49:34 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -70,12 +70,12 @@ function stringToArray(string) {
 
 /*VitaliiMatviienko------------*/
 
-/*https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript*/
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
   return dna.replace(/T/g,"U");
 }
 
-https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 var min = function(list){
     
     return Math.min.apply(null,list);
