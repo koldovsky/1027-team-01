c1fa5386025b16fa1cd16d7639b11baceed52739
Taras Kravetskyi <ckjym@ukr.net>
2023-06-21 12:14:32 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -153,6 +153,18 @@ function DNAtoRNA(dna) {
   return array.join("");
 }
 
+//3--Find Maximum and Minimum Values of a List--
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list){
+    return Math.min(...list);
+}
+
+var max = function(list){
+    return Math.max(...list);
+}
+
+
+
 
 
 
