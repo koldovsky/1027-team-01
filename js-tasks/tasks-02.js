5f5bd4ea2a07206858cec3b0bf8ffe7ae4297dc0
Taras Kravetskyi <ckjym@ukr.net>
2023-06-21 12:58:41 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -185,6 +185,21 @@ function min(arr, toReturn) {
   }
 }
 
+//5--A wolf in sheep's clothing--
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf("wolf");
+  if (position === 0) {
+    return "Pls go away and stop eating my sheep";
+  } else {
+    return `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
+  }
+}
+
+
+
+
+
 
 
 
