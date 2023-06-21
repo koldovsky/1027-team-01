544886d994500f9a3f9b2e8fe4fa4452e543ee5e
Taras Kravetskyi <ckjym@ukr.net>
2023-06-21 14:30:00 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -162,9 +162,9 @@ function min(arr, toReturn) {
 
 //Taras Kravetskyi
 //1--Convert a string to an array--
-//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript 
-function stringToArray(string){
-return string.split(' ');
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+  return string.split(" ");
 }
 
 //2--DNA to RNA Conversion--
@@ -181,19 +181,19 @@ function DNAtoRNA(dna) {
 
 //3--Find Maximum and Minimum Values of a List--
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-var min = function(list){
-    return Math.min(...list);
-}
+var min = function (list) {
+  return Math.min(...list);
+};
 
-var max = function(list){
-    return Math.max(...list);
-}
+var max = function (list) {
+  return Math.max(...list);
+};
 
 //4--Smallest value of an array--
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 function min(arr, toReturn) {
-  const minNumber = Math.min(...arr)
-  
+  const minNumber = Math.min(...arr);
+
   if (toReturn === "value") {
     return minNumber;
   } else if (toReturn === "index") {
@@ -214,7 +214,7 @@ function warnTheSheep(queue) {
 
 //6--Beginner - Lost Without a Map--
 //https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
-function maps(x){
+function maps(x) {
   return x.map((doubleNum) => doubleNum * 2);
 }
 
