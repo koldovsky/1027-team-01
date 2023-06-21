8c815665bacdbda7f2b998e6bc402997685e5283
Taras Kravetskyi <ckjym@ukr.net>
2023-06-21 14:26:54 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -218,6 +218,18 @@ function maps(x){
   return x.map((doubleNum) => doubleNum * 2);
 }
 
+//7--Find the first non-consecutive number--
+//https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1];
+    }
+  }
+  return null;
+}
+
+
 
 
 
