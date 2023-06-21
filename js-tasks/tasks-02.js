063eabd13a27683154a7f40cb8dc4907d5cb0b97
Taras Kravetskyi <ckjym@ukr.net>
2023-06-21 13:30:06 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -212,6 +212,12 @@ function warnTheSheep(queue) {
   }
 }
 
+//6--Beginner - Lost Without a Map--
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+function maps(x){
+  return x.map((doubleNum) => doubleNum * 2);
+}
+
 
 
 
