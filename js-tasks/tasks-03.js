3e3c71ee68841d6fbfa2283cb41a41e312993d2b
Bogdan <bgra4ov@gmail.com>
2023-06-22 18:41:50 +0300

    three tasks done

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -14,4 +14,34 @@ function circleCircumference(circle) {
   return Number(circumference.toFixed(6));
 }
 
+/*Grachov Bogdan*/
+
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript ( Find the smallest integer in the array )
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript ( Geometry Basics: Circle Circumference in 2D )
+
+function circleCircumference(circle) {
+  const circumference = 2 * Math.PI * circle.radius;
+  return circumference;
+    }
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript (Training JS #12: loop statement --for..in and for..of)
+
+function giveMeFive(obj) {
+  const resultArray = [];
+
+for(let key in obj){
+  if(key.length === 5) resultArray.push(key);
+  if(obj[key].length === 5) resultArray.push(obj[key])
+}
+  
+  return resultArray;
+}
+
 
