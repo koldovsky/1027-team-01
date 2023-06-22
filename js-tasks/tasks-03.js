6469b64da98b943d1738a81a7e9695b7c29e249a
Taras Kravetskyi <ckjym@ukr.net>
2023-06-22 17:59:52 +0300

    Update tasks-03.js

---
+++ js-tasks/tasks-03.js
@@ -0,0 +1,8 @@
+// 1 Find the smallest integer in the array
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
