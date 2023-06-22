47d667e57f185c7d84aa8f895f5cce2b35dbcd54
Taras Kravetskyi <ckjym@ukr.net>
2023-06-22 18:30:15 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -6,3 +6,12 @@ class SmallestIntegerFinder {
     return Math.min(...args);
   }
 }
+
+// 2 Geometry Basics: Circle Circumference in 2D
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  const circumference = 2 * Math.PI * circle.radius;
+  return Number(circumference.toFixed(6));
+}
+
+
