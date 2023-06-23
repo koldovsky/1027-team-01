76f4485d4bac87c3bbf76c4e7ec13147577b93df
Taras Kravetskyi <ckjym@ukr.net>
2023-06-23 12:31:08 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -55,6 +55,35 @@ function buildFun(n) {
   return Array.from({ length: n }, (ignored, index) => () => index);
 }
 
+
+// 5 Fun with ES6 Classes #2 - Animals and Inheritance
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
+
 /*Grachov Bogdan*/
 
 // https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript ( Find the smallest integer in the array )
