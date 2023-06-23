db0543f45d3e32a5d023e034e5138ee8a208b979
GUID <vitalijmatvienko@gmail.com>
2023-06-23 11:13:18 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -86,3 +86,8 @@ class SmallestIntegerFinder {
     return Math.min(...args);
   }
 }
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
