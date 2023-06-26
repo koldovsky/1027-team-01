ad09b2e6630fd9912d668ac7113c17f168bad323
fedor-bratishkin <128460052+fedor-bratishkin@users.noreply.github.com>
2023-06-26 18:15:33 +0300

    Update index.js
    Delete Accomm JS

--- js/index.js
+++ js/index.js
@@ -3,7 +3,5 @@ document.addEventListener('partialsLoaded', () => {
     import('./rentals.js');
     import('./shop.js');
     import('./getfullanswer-title-running.js');
-    import('./accommodation-list.js');
-    import('./accomodation-carousel.js');
     import('./outfacilities-clock.js');
 });
