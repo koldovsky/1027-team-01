1490c92ca8afb875b5e1efdf031cbd0e09bfdecf
fedor-bratishkin <128460052+fedor-bratishkin@users.noreply.github.com>
2023-06-26 10:40:08 +0000

    Fix accomm

--- js/accomodation-carousel.js
+++
@@ -1,4 +0,0 @@
-const slides = [
-    '<div><img src="../img/accommodation/accommodation_river.jpeg" alt ="River"></div>',
-    '<div><img src="../img/accommodation/accommodation_lodge.jpeg" alt ="Lodge"></div>',
-]
