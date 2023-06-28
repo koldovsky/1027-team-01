364de8ce47d3570e3a82655d00119935359d58fa
fedor-bratishkin <128460052+fedor-bratishkin@users.noreply.github.com>
2023-06-28 19:46:20 +0000

    Currency

--- js/accommodation-list.js
+++ js/accommodation-list.js
@@ -1,90 +1,52 @@
-console.log('Secondary page JavaScript code');
+let accommodation; 
 
+fetch('api/accommodation.json')
+  .then(response => response.json())
+  .then(data => {
+    accommodation = data; 
+    renderAccommodation(accommodation, 1);
+    cardBackground();
+  });
 
-document.addEventListener('partialsLoaded', function () {
-    console.log('Partials loaded');
-});
+function renderAccommodation(accommodation, rate) {
+  const accommodationList = document.querySelector('.accommodation__container');
+  accommodationList.innerHTML = '';
 
-const accommodation = [
-    {
-        id: '1',
-        price: '65',
-        name: 'River Chalet',
-        image: 'img/accommodation/accommodation_river.jpeg',
-        info: '1 Bedroom, 1 Bathroom, Sleeps 2'
-    },
-    {
-        id: '2',
-        price: '75',
-        name: 'Lookout Lodge',
-        image: 'img/accommodation/accommodation_lodge.jpeg',
-        info: '2 Bedrooms, 1 Bathroom, Sleeps 4'
-    },
-    {
-        id: '3',
-        price: '85',
-        name: 'Smoky View Chalet',
-        image: 'img/accommodation/accommodation_smoky_view_chalet.jpeg',
-        info: '2 Bedrooms,2 Bathrooms, Sleeps 6'
-    },
-    {
-        id: '4',
-        price: '100',
-        name: 'Mountain Haven',
-        image: 'img/accommodation/accommodation_haven.jpeg',
-        info: '3 Bedrooms,2 Bathrooms, Sleeps 8'
-    },
-    {
-        id: '5',
-        price: '150',
-        name: 'Mountain Splash',
-        image: 'img/accommodation/accommodation_mountain_splash.jpeg',
-        info: '3 Bedrooms,3 Bathrooms, Sleeps 10'
-    }
-];
-
-function renderAccommodation(accommodation) {
-    const accommodationList = document.querySelector('.accommodation__container');
-    accommodationList.innerHTML = '';
-
-    for (const item of accommodation) {
-        accommodationList.innerHTML += `
-        <article class="accommodation__card card">
+  for (const item of accommodation) {
+    accommodationList.innerHTML += `
+      <article class="accommodation__card card">
         <div class="block">
-            <div class="card__image">
-                <img src="${item.image}" alt="${item.name}">
+          <div class="card__image">
+            <img src="${item.image}" alt="${item.name}">
+          </div>
+          <div class="card__inform">
+            <p class="card__price"><span class="price" style="color: green">${(item.price * rate).toFixed(2)}</span>/night</p>
+            <h3 class="card__name">${item.name}</h3>
+            <p class="card__info">${item.info}</p>
+            <div class="card__button button">
+              <a class="button" href="#">
+                Reserve cabin
+              </a>
             </div>
-            <div class="card__inform">
-                <p class="card__price"><span class="price" style="color: green">$${item.price}</span>/night</p>
-                <h3 class="card__name">${item.name}</h3>
-                <p class="card__info">${item.info}</p>
-                <div class="card__button button">
-                    <a class="button" href="#">
-                        Reserve cabin
-                    </a>
-                </div>
-            </div>
-            </div>
-        </article>
-        `;
-    }
+          </div>
+        </div>
+      </article>
+    `;
+  }
 }
 
-renderAccommodation(accommodation);
+let rates;
 
-function toggleCardBackground() {
-    const cards = document.querySelectorAll('.accommodation__card');
-    cards.forEach((card, index) => {
-        if (index % 2 === 0) {
-            card.classList.add('light-background');
-            card.classList.remove('dark-background');
-        } else {
-            card.classList.add('dark-background');
-            card.classList.remove('light-background');
-        }
-    });
+async function changeCurrency() {
+  if (!rates) {
+    const response = await fetch('https://api.exchangerate-api.com/v4/latest/usd');
+    rates = (await response.json()).rates;
+  }
+  
+  const currentCurrency = document.querySelector('.currency').value;
+  const rate = rates[currentCurrency];
+  renderAccommodation(accommodation, rate);
+  cardBackground();
 }
 
-// Вызов функции для переключения цвета фона карточек
-toggleCardBackground();
-
+document.querySelector('.currency').addEventListener('change', changeCurrency);
