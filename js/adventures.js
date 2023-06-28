025a81f95df62f941eed6294aa72da295e0b6d61
psychomantisua <99193539+psychomantisua@users.noreply.github.com>
2023-06-28 23:09:14 +0000

    js file added

--- js/adventures.js
+++ js/adventures.js
@@ -1,31 +1,20 @@
-// Получаем ссылку на картинку
-var image = document.getElementById('myImage');
+var images = document.getElementsByClassName("adventures__places-image");
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", enlargeImage);
+}
 
-// Устанавливаем начальный размер картинки
-var targetWidth = 500; // Ширина в пикселях
-var targetHeight = 400; // Высота в пикселях
+function enlargeImage() {
+  var overlay = document.createElement('div');
+  overlay.className = 'overlay';
 
-// Увеличиваем картинку на весь экран при клике
-image.addEventListener('click', function() {
-  image.style.transform = 'translate3d(-124px, 0px, 0px) scale3d(0.637596, 0.637596, 1)';
-  //image.style.width = targetWidth + 'px';
-  //image.style.height = targetHeight + 'px';
-  image.style.display = 'block';
-  image.style.margin = '0 auto';
-  image.style.position = 'fixed';
-  image.style.top = '50%';
-  image.style.left = '50%';
-  image.style.transform = 'translate(-50%, -50%)';
-  image.style.zIndex = '9999';
+  var enlargedImage = document.createElement('img');
+  enlargedImage.src = this.src;
+  enlargedImage.className = 'enlarged-image';
 
-    
-});
+  overlay.appendChild(enlargedImage);
+  document.body.appendChild(overlay);
 
-// Восстанавливаем размер картинки после выхода из полноэкранного режима
-document.addEventListener('fullscreenchange', function() {
-  if (!document.fullscreenElement) {
-    image.style.width = originalWidth + 'px';
-    image.style.height = originalHeight + 'px';
-  }
-});
-
+  overlay.onclick = function() {
+    document.body.removeChild(overlay);
+  };
+}
--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 document.addEventListener('partialsLoaded', () => {
-<<<<<<< HEAD
     import('../js/burger-menu.js');
     import('../js/rentals.js');
     import('../js/shop.js');
     import('./testimonials.js');
     import('../js/adventures.js');
-});
-=======
     import('./burger-menu.js');
     import('./rentals.js');
     import('./rentals.js');
@@ -14,4 +11,3 @@ document.addEventListener('partialsLoaded', () => {
     import('./outfacilities-clock.js');
     import('./testimonials.js');
 });
->>>>>>> 364de8ce47d3570e3a82655d00119935359d58fa
