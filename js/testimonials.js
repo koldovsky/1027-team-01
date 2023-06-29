50f24f6e0b5bf72551efbffdbda2782f28e1bef1
Bogdan <bgra4ov@gmail.com>
2023-06-29 22:55:37 +0300

    testimonials prettier check

--- js/testimonials.js
+++ js/testimonials.js
@@ -2,28 +2,29 @@
 let letters = document.querySelector(".testimonials__title h2").innerHTML.split("");
 
 const colors = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'lightblue',
-  'blue',
-  'purple'
-]
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "lightblue",
+  "blue",
+  "purple",
+];
 
 const getRandomColor = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 Array.prototype.randomColor = function () {
   let h2 = "";
-  this.map(letter => {
+  this.map((letter) => {
     let color = getRandomColor(colors);
     h2 += '<span style="color:' + color + '">' + letter + "</span>";
   });
   return h2;
 };
 
-function setColor(){
-document.querySelector(".testimonials__title h2").innerHTML = letters.randomColor();
+function setColor() {
+  document.querySelector(".testimonials__title h2").innerHTML =
+    letters.randomColor();
 }
 
 setInterval(setColor, 100);
