92fd35db11968efd555f1d6f277b18ff1c83426e
Bogdan <bgra4ov@gmail.com>
2023-06-30 11:57:45 +0300

    testimonials fetch

--- js/testimonials.js
+++ js/testimonials.js
@@ -28,3 +28,20 @@ function setColor() {
 }
 
 setInterval(setColor, 100);
+
+/*JSON Fetch*/
+
+const response = await fetch('api/testimonials.json');
+const images = await response.json();
+renderProducts(images);
+
+function renderProducts(images) {
+    const testimonialsGallery = document.querySelector('.testimonials__gallery');
+    testimonialsGallery.innerHTML = '';
+    
+    for (const image of images) {
+      testimonialsGallery.innerHTML += `
+        <img src="${image.src}" class="${image.class}" alt="${image.alt}">
+        `
+    }
+}
