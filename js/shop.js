7ad15959cbe7081b31ef25b816b5f7b958994434
Yuliia <alice1yulia@gmail.com>
2023-06-30 12:08:34 +0200

    add API(adviceslip) to js shop

--- js/shop.js
+++ js/shop.js
@@ -1,16 +1,36 @@
-const images = document.querySelectorAll('.image-card__image');
+function animateImages() {
+  const images = document.querySelectorAll('.image-card__image');
 
-Array.from(images).forEach(function(elem) {
-  elem.addEventListener('mouseenter', (event) => {
-    const image = event.target;
-    const newSrc = event.target.dataset.hover;
-    image.src = newSrc;
+  Array.from(images).forEach(function (elem) {
+    elem.addEventListener('mouseenter', (event) => {
+      const image = event.target;
+      const newSrc = event.target.dataset.hover;
+      image.src = newSrc;
+    });
+  
+    elem.addEventListener('mouseleave', (event) => {
+      console.log('mouseleave', event.target.dataset);
+      const image = event.target;
+      const newSrc = event.target.dataset.original;
+      image.src = newSrc;
+    });
   });
+}
 
-  elem.addEventListener('mouseleave', (event) => {
-    console.log('mouseleave', event.target.dataset);
-    const image = event.target;
-    const newSrc = event.target.dataset.original;
-    image.src = newSrc;
-  });
-});
+
+function displayAdvice() {
+  const adviceElement = document.querySelector('.adviceText');
+
+  async function displayRandomAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    const adviceText = data.slip.advice;
+    adviceElement.textContent = adviceText;
+  }
+  
+  displayRandomAdvice();
+  adviceElement.addEventListener('click', displayRandomAdvice);
+}
+
+animateImages();
+displayAdvice();
