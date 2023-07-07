6468bdd0c75978d83961f18047b47ee24bdcbacf
VitaliiMatviienko <vitalijmatvienko@gmail.com>
2023-07-07 12:49:54 +0300

    update photo-gallery

--- js/photo-gallery-by-click-link.js
+++ js/photo-gallery-by-click-link.js
@@ -5,7 +5,6 @@ const autumnLink = document.getElementById("autumn-link");
 const fishingLink = document.getElementById("fishing-link");
 const spaLink = document.getElementById("spa-link");
 
-
 const summerContainer = document.querySelector(".photo-gallery__container--summer");
 const winterContainer = document.querySelector(".photo-gallery__container--winter");
 const springContainer = document.querySelector(".photo-gallery__container--spring");
@@ -13,43 +12,47 @@ const autumnContainer = document.querySelector(".photo-gallery__container--autum
 const fishingContainer = document.querySelector(".photo-gallery__container--fishing");
 const spaContainer = document.querySelector(".photo-gallery__container--spa");
 
-
 const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const randomLink = getRandom([summerLink, winterLink, springLink, autumnLink, fishingLink, spaLink]);
 
-
 summerLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(summerLink);
   showContainer(summerContainer);
   hideContainers([winterContainer, springContainer, autumnContainer, fishingContainer, spaContainer]);
 });
 
 winterLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(winterLink);
   showContainer(winterContainer);
   hideContainers([summerContainer, springContainer, autumnContainer, fishingContainer, spaContainer]);
 });
 
 springLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(springLink);
   showContainer(springContainer);
   hideContainers([summerContainer, winterContainer, autumnContainer, fishingContainer, spaContainer]);
 });
 
 autumnLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(autumnLink);
   showContainer(autumnContainer);
   hideContainers([summerContainer, winterContainer, springContainer, fishingContainer, spaContainer]);
 });
 
 fishingLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(fishingLink);
   showContainer(fishingContainer);
   hideContainers([summerContainer, winterContainer, springContainer, autumnContainer, spaContainer]);
 });
 
 spaLink.addEventListener("click", function(event) {
   event.preventDefault();
+  setActiveLink(spaLink);
   showContainer(spaContainer);
   hideContainers([summerContainer, winterContainer, springContainer, autumnContainer, fishingContainer]);
 });
@@ -61,9 +64,16 @@ function showContainer(container) {
   container.style.display = "flex";
 }
 
-
 function hideContainers(containers) {
   containers.forEach(function(container) {
     container.style.display = "none";
   });
 }
+
+function setActiveLink(link) {
+    const navItems = document.querySelectorAll('.photo-gallery__nav-item');
+    navItems.forEach(item => {
+      item.classList.remove('active');
+    });
+    link.classList.add('active');
+  }
