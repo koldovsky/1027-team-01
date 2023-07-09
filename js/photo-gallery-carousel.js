28f7f8861de744adc4eb327bb3719e4cfdd37302
VitaliiMatviienko <vitalijmatvienko@gmail.com>
2023-07-09 16:39:32 +0300

    update photo-gallery

--- js/photo-gallery-carousel.js
+++ js/photo-gallery-carousel.js
@@ -1,4 +1,3 @@
-
 const carousel = document.querySelector('.our-guests__carousel');
 const carouselSlide = document.querySelector('.our-guests__carousel-slide');
 const prevButton = document.querySelector('.our-guests__carousel-btn-prev');
@@ -10,38 +9,38 @@ let currentIndex = slidesPerView;
 setupCarousel();
 
 function getSlidesPerView() {
-   if (window.innerWidth >= 1281) return 5;
-   if (window.innerWidth >= 961) return 4;
-   if (window.innerWidth >= 641) return 3;
-   if (window.innerWidth >= 321) return 2;
-   return 1;
+  if (window.innerWidth >= 1281) return 5;
+  if (window.innerWidth >= 961) return 4;
+  if (window.innerWidth >= 641) return 3;
+  if (window.innerWidth >= 321) return 2;
+  return 1;
 }
 
 function setupCarousel() {
-    // Remove clones if they exist
-    slides = slides.filter(slide => !slide.classList.contains('clone'));
+  // Remove clones if they exist
+  slides = slides.filter(slide => !slide.classList.contains('clone'));
 
-    // Add clones at start and end for infinite looping
-    const clonesStart = slides.slice(-slidesPerView).map(cloneSlide);
-    const clonesEnd = slides.slice(0, slidesPerView).map(cloneSlide);
+  // Add clones at start and end for infinite looping
+  const clonesStart = slides.slice(-slidesPerView).map(cloneSlide);
+  const clonesEnd = slides.slice(0, slidesPerView).map(cloneSlide);
 
-    // Add all slides to the carousel
-    carouselSlide.append(...clonesStart, ...slides, ...clonesEnd);
+  // Add all slides to the carousel
+  carouselSlide.append(...clonesStart, ...slides, ...clonesEnd);
 
-    // Update slides
-    slides = Array.from(carouselSlide.children);
+  // Update slides
+  slides = Array.from(carouselSlide.children);
 
-    updateCarousel();
+  updateCarousel();
 }
 
 function cloneSlide(slide) {
-    const clone = slide.cloneNode(true);
-    clone.classList.add('clone');
-    return clone;
+  const clone = slide.cloneNode(true);
+  clone.classList.add('clone');
+  return clone;
 }
 
 function updateCarousel() {
-    carouselSlide.style.transform = `translateX(-${currentIndex * 100 / slidesPerView}%)`;
+  carouselSlide.style.transform = `translateX(-${currentIndex * 100 / slidesPerView}%)`;
 }
 
 // Event listeners
@@ -49,120 +48,38 @@ prevButton.addEventListener('click', () => {
   currentIndex--;
   if (currentIndex < 0) {
     currentIndex = slides.length - slidesPerView;
-    setTransitionNone();
-    updateCarousel();
-  } else {
-    updateCarousel();
   }
+  setTransitionNone();
+  updateCarousel();
 });
 
 nextButton.addEventListener('click', () => {
-    currentIndex++;
-    if (currentIndex > slides.length - slidesPerView) {
-        currentIndex = slidesPerView;
-        setTransitionNone();
-    }
-    updateCarousel();
+  currentIndex++;
+  if (currentIndex > slides.length - slidesPerView) {
+    currentIndex = slidesPerView;
+  }
+  setTransitionNone();
+  updateCarousel();
 });
 
 window.addEventListener('resize', () => {
-    slidesPerView = getSlidesPerView();
-    setupCarousel();
+  slidesPerView = getSlidesPerView();
+  setupCarousel();
 });
 
 // Function to set transition to 'none' temporarily
 function setTransitionNone() {
-    carouselSlide.style.transition = 'none';
-    updateCarousel();
-    // Allow transition to complete, then reset transition style
+  carouselSlide.style.transition = 'none';
+  updateCarousel();
+  // Allow transition to complete, then reset transition style
+  requestAnimationFrame(() => {
     requestAnimationFrame(() => {
-        requestAnimationFrame(() => {
-            carouselSlide.style.transition = '';
-        });
+      carouselSlide.style.transition = '';
     });
+  });
 }
 
-//gpt code
-// const carousel = document.querySelector('.our-guests__carousel');
-// const carouselSlide = document.querySelector('.our-guests__carousel-slide');
-// const prevButton = document.querySelector('.our-guests__carousel-btn-prev');
-// const nextButton = document.querySelector('.our-guests__carousel-btn-next');
 
-// let slidesPerView = getSlidesPerView();
-// let slides = Array.from(carouselSlide.children);
-// let currentIndex = slidesPerView;
-// setupCarousel();
-
-// function getSlidesPerView() {
-//    if (window.innerWidth >= 1281) return 5;
-//    if (window.innerWidth >= 961) return 4;
-//    if (window.innerWidth >= 641) return 3;
-//    if (window.innerWidth >= 321) return 2;
-//    return 1;
-// }
-
-// function setupCarousel() {
-//     // Remove clones if they exist
-//     slides = slides.filter(slide => !slide.classList.contains('clone'));
-
-//     // Add clones at start and end for infinite looping
-//     const clonesStart = slides.slice(-slidesPerView).map(cloneSlide);
-//     const clonesEnd = slides.slice(0, slidesPerView).map(cloneSlide);
-
-//     // Add all slides to the carousel
-//     carouselSlide.append(...clonesStart, ...slides, ...clonesEnd);
-
-//     // Update slides
-//     slides = Array.from(carouselSlide.children);
-
-//     updateCarousel();
-// }
-
-// function cloneSlide(slide) {
-//     const clone = slide.cloneNode(true);
-//     clone.classList.add('clone');
-//     return clone;
-// }
-
-// function updateCarousel() {
-//     carouselSlide.style.transform = `translateX(-${currentIndex * 100 / slidesPerView}%)`;
-// }
-
-// // Event listeners
-// prevButton.addEventListener('click', () => {
-//     currentIndex--;
-//     if (currentIndex < 0) {
-//         currentIndex = slides.length - slidesPerView * 2 - 1;
-//         setTransitionNone();
-//     }
-//     updateCarousel();
-// });
-
-// nextButton.addEventListener('click', () => {
-//     currentIndex++;
-//     if (currentIndex > slides.length - slidesPerView) {
-//         currentIndex = slidesPerView;
-//         setTransitionNone();
-//     }
-//     updateCarousel();
-// });
-
-// window.addEventListener('resize', () => {
-//     slidesPerView = getSlidesPerView();
-//     setupCarousel();
-// });
-
-// // Function to set transition to 'none' temporarily
-// function setTransitionNone() {
-//     carouselSlide.style.transition = 'none';
-//     updateCarousel();
-//     // Allow transition to complete, then reset transition style
-//     requestAnimationFrame(() => {
-//         requestAnimationFrame(() => {
-//             carouselSlide.style.transition = '';
-//         });
-//     });
-// }
 
 //mentor code
 // const carousel = document.querySelector('.our-guests__carousel');
