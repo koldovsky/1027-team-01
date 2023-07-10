7c7461cb003554f5cfe9f5051f8f3bfffd9fc66c
cyberfoxie <116913117+cyberfoxie@users.noreply.github.com>
2023-07-10 19:05:40 +0300

    Update orderacall.js

--- js/orderacall.js
+++ js/orderacall.js
@@ -1,59 +1,61 @@
-// // document.querySelectorAll('.md-textbox')
-//     .forEach( cb => {
-//         cb.addEventListener('change', ev => {
-//             if (ev.target.value !== '') {
-//                 ev.target.classList.add('has-value')
-//             } else {
-//                 ev.target.classList.remove('has-value')
-//             }
-//         })
-//     })
-// var modal = document.getElementById("modal");
-// var button = document.querySelector(".container-call button");
-// var closeButton = document.getElementsByClassName("close")[0];
-// var okButton = document.getElementById("okButton");
+document.querySelectorAll('.md-input')
+    .forEach(cb => {
+        cb.addEventListener('change', ev => {
+            if (ev.target.value !== '') {
+                ev.target.classList.add('has-value')
+            } else {
+                ev.target.classList.remove('has-value')
+            }
+        })
+    })
 
-// button.onclick = function() {
-//     modal.style.display = "block";
-// }
+const modal = document.getElementById("modal");
+const button = document.querySelector(".container-call button");
+const closeButton = document.getElementsByClassName("close")[0];
+const okButton = document.getElementById("okButton");
 
-// closeButton.onclick = function() {
-//     modal.style.display = "none";
-// }
+button.onclick = () => {
+    modal.style.display = "block";
+}
 
-// okButton.onclick = function() {
-//     modal.style.display = "none";
-// }
-// function centerModalContent() {
-//     var modalContent = document.querySelector(".modal-content");
-//     modalContent.style.marginTop = (window.innerHeight - modalContent.offsetHeight) / 2 + "px";
-// }
-// //текст зникає //
-// const nameInput = document.getElementById("input-name");
-// const nameLabel = document.querySelector('.md-textbox label[for="input-name"]');
-// const phoneInput = document.getElementById("input-phone");
-// const phoneLabel = document.querySelector('.md-textbox label[for="input-phone"]');
+closeButton.onclick = () => {
+    modal.style.display = "none";
+}
 
-// nameInput.addEventListener('input', () => {
-//     if (nameInput.value !== '') {
-//         nameLabel.style.display = 'none';
-//     } else {
-//         nameLabel.style.display = 'inline';
-//     }
-// });
+okButton.onclick = () => {
+    modal.style.display = "none";
+}
 
-// phoneInput.addEventListener('input', () => {
-//     if (phoneInput.value !== '') {
-//         phoneLabel.style.display = 'none';
-//     } else {
-//         phoneLabel.style.display = 'inline';
-//     }
-// });
-// okButton.onclick = function() {
-//     modal.style.display = "none";
-//     nameInput.value = "";
-//     phoneInput.value = "";
-//     nameLabel.style.display = "inline";
-//     phoneLabel.style.display = "inline";
-// }
-// //
+function centerModalContent() {
+    const modalContent = document.querySelector(".modal-content");
+    modalContent.style.marginTop = (window.innerHeight - modalContent.offsetHeight) / 2 + "px";
+}
+
+const nameInput = document.getElementById("input-names");
+const nameLabel = document.querySelector('.md-input label[for="input-names"]');
+const phoneInput = document.getElementById("input-phones");
+const phoneLabel = document.querySelector('.md-input label[for="input-phones"]');
+
+nameInput.addEventListener('input', () => {
+    if (nameInput.value !== '') {
+        nameLabel.style.display = 'none';
+    } else {
+        nameLabel.style.display = 'inline';
+    }
+});
+
+phoneInput.addEventListener('input', () => {
+    if (phoneInput.value !== '') {
+        phoneLabel.style.display = 'none';
+    } else {
+        phoneLabel.style.display = 'inline';
+    }
+});
+
+okButton.onclick = () => {
+    modal.style.display = "none";
+    nameInput.value = "";
+    phoneInput.value = "";
+    nameLabel.style.display = "inline";
+    phoneLabel.style.display = "inline";
+}
